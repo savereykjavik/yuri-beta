c91510c5b4321591c8a1767998f0d6be30b6fec4
Caroline Thordenberg <caroline.thordenberg@gmail.com>
2015-12-10 15:10:52 +0100

    color

--- js/Canon EOS Rebel T5i_700D For Dummies Cheat Sheet - For Dummies_files/a.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  document.write('<div id="adsatlas-1157488496" style="position:relative;width:160px;height:600px;overflow:hidden">');
-  document.write('<iframe src=\"https://ad.atdmt.com/as/direct;p=11087204610782;as=0;a=11087204610669;crs=11087204610644;cr=11087204610645;i.ts=1448714179/O365_TheNewOffice_NC_NL-NL_160x600_BAN_v2.html?click=http://a.rfihub.com/acs/b/c3Q9aHRtbCZhYT0xODk0NTkxLDE2NjU1Nzk1LDg0MTcwMSwzNDg3OTE1OSw2MzQ4OSw1NjE1MTMsNTM4OTYyOGQ0OWZhNzNhYjE1NjkyYzJjMGYyNDU0OWQscCwyNzcsMjM4NzU5LDE3ODMyMDIzLDE3NDI1Nyw0MzUyMjUmbXQ9MSZyYj02NCZyZT0zNDcwMyZoY2k9MTU2NDI3ODYyNTYyOTg0NjQ0NSZ1dWlkPTEwNDEyNDYzMTA1NDg2NTg0NzMmZGk9JmRjPTMmZGlzcmM9MA../n/\" width=\"160\" height=\"600\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" topmargin=\"0\" leftmargin=\"0\" allowtransparency=\"true\"></iframe>');
-  document.write('</div>');
-  document.write('');
-  document.close();
-})();
--- js/Canon EOS Rebel T5i_700D For Dummies Cheat Sheet - For Dummies_files/ca-pub-5431236916788064.js
+++
@@ -1,1 +0,0 @@
-(google_async_config = window.google_async_config || {})['ca-pub-5431236916788064'] = {"sra_enabled":false};
